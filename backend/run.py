19dcaabd23390e3ae65334bc3d22ab4cf9aac869
Lucas Soares de Oliveira <lukas.s.o@hotmail.com>
2025-08-25 17:14:28 -0300

    Create app folder with __init__.py and make small changes to run.py

--- backend/run.py
+++ backend/run.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-    return "huhuhuu, não?"
+from app import create_app
+app = create_app()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
